df5c0b2235da48abf0ea822c379132d91cae784f
Raimundo <ray.yh.yh@gmail.com>
2026-08-15 04:18:34 -0300

    feat: configuração main e testes

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
+from Piece import Piece
+from Box import Box
+from ursina import Ursina
+from ursina import EditorCamera
+
+
+def input(key):
+    match key:
+        case "space":
+             print("space")
 def main():
-    print("Hello from te3d!")
+    
+    # gerando o app
+    app = Ursina("te3d")
+
+    # habilitando camera
+    _ec = EditorCamera(
+        rotation_speed=300,
+        rotation_smoothing=10,
+    )
+        
+    # gerando a caixa de limites
+    box = Box(16)
+    box.build()
+
+    piece = Piece("Z")
+    piece.build()
+
+    # rodando app
+    app.run()
+    return
 
 
 if __name__ == "__main__":
